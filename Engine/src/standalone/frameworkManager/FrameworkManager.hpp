bcaba227d952888e114ab6197a1616eb26b9cc74
Marc-Antoine Leconte <marc-antoine.leconte@epitech.eu>
2017-11-03 11:33:22 +0100

    FrameworkReflexion(en): split FrameworkManager.hpp in a .cpp

--- Engine/src/standalone/frameworkManager/FrameworkManager.hpp
+++ Engine/src/standalone/frameworkManager/FrameworkManager.hpp
@@ -42,81 +42,35 @@ public:
 	*	@param	frameworkName	The name of the framework to load.
 	*	@param	debug			Allow or not debug prints.
 	*/
-	explicit FrameworkManager(const std::string & frameworkName, bool debug = false) :
-		_loader(std::string(frameworkName), debug),
-		_crawler(std::string("./frameworks/"), debug),
-		_frameworkName(frameworkName),
-		_framework(nullptr)
-	{
-		this->checkFile();
-		this->loadLib();
-	}
+	explicit						FrameworkManager(const std::string & frameworkName, bool debug = false);
 
 	/**
 	*	Destructor.
 	*/
-	virtual ~FrameworkManager(void)
-	{
-	}
+	virtual							~FrameworkManager(void);
 
 private:
 	/**
 	*	Function checking if the framework library is present, at the right format and the right place. Called in the constructor only.
 	*/
-	void							checkFile(void)
-	{
-		std::string					tmp;
-		size_t						nameSize;
-
-		auto pathList = this->_crawler.getEntriesListByType(nx::Crawler::REGULAR);
-
-		if (pathList.size() <= 0)
-			throw nx::FrameworkException("Can't find dynamic librarys in ./framework");
-
-		for (auto it : pathList)
-		{
-			tmp = it.string();
-			nameSize = DYNAMIC_LIBRARY_EXTENSION.size() + this->_frameworkName.size();
-			if (tmp.size() >= nameSize && tmp.substr(tmp.size() - nameSize) == this->_frameworkName + DYNAMIC_LIBRARY_EXTENSION)
-				return;
-		}
-		throw nx::FrameworkException("Can't find " + this->_frameworkName + DYNAMIC_LIBRARY_EXTENSION + " in ./framework");
-	}
+	void							checkFile(void);
 
 	/**
 	*	Function allowing to load the framework library. Called in constructor only.
 	*/
-	void			loadLib(void)
-	{
-		try
-		{
-			this->_loader.addLib(std::string(this->_frameworkName + DYNAMIC_LIBRARY_EXTENSION));
-		}
-		catch (const nx::DLLoaderException &err)
-		{
-			throw nx::FrameworkException(err.what());
-		}
-		if ((this->_framework = this->_loader.getInstance(std::string(this->_frameworkName + DYNAMIC_LIBRARY_EXTENSION))) == nullptr)
-			throw nx::FrameworkException("Can't create an instance from the framework " + this->_frameworkName + ".");
-	}
+	void							loadLib(void);
 
 public:
 	/**
 	*	Function of print. Permit to display the library loaded in the FrameworkManager.
 	*/
-	void			dump(void) const
-	{
-		this->_loader.dump();
-	}
+	void							dump(void) const;
 
 	/**
 	*	Get an instance of the current framework.
 	*	@return Return an instance of the framework.
 	*/
-	std::shared_ptr<FrameworkTpl>	getFramework(void) const
-	{
-		return (this->_framework);
-	}
+	std::shared_ptr<FrameworkTpl>	getFramework(void) const;
 };
 
 #endif /*FRAMEWORK_MANAGER_HPP_*/
