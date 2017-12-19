6ad8ced3441a0f2ddfada5e05d286fc949f42217
Maxime Cauvin <maxime.cauvin@epitech.eu>
2017-12-19 11:58:25 +0100

    FrameworkRendering(feat): Adding working Rendering framework with SFML

---
+++ Engine/frameworks/rendering/src/FrameworkRendering.cpp
@@ -0,0 +1,28 @@
+#include "FrameworkRendering.hpp"
+
+FrameworkRendering::FrameworkRendering(nx::Engine* engine)
+:
+	nx::RenderingFrameworkTpl("FrameworkRendering"),
+	_engine(engine)
+{
+	std::cout << "New Rendering Framework created" << std::endl;
+}
+
+FrameworkRendering::~FrameworkRendering()
+{
+	std::cout << "New Rendering Framework deleted" << std::endl;
+}
+
+void FrameworkRendering::TriggerRenderingEvent()
+{
+	sf::Window App(sf::VideoMode(800, 600), "myproject");
+
+	while (App.isOpen()) {
+		sf::Event Event;
+		while (App.pollEvent(Event)) {
+			if (Event.type == sf::Event::Closed)
+				App.close();
+		}
+		App.display();
+	}
+}
---
+++ Engine/includes/Nexus/rendering.hpp
@@ -0,0 +1,38 @@
+#ifndef NEXUS_ENGINE__RENDERING_SYSTEM_HPP_
+#define NEXUS_ENGINE__RENDERING_SYSTEM_HPP_
+
+#include <iostream>
+#include <memory>
+
+#include "Nexus/systems/SystemTpl.hpp"
+#include "Nexus/frameworks/RenderingFrameworkTpl.hpp"
+
+namespace nx {
+	template <typename T>
+	class FrameworkManager;
+}
+
+namespace nx {
+	class RenderingSystem : public SystemTpl
+	{
+		# define __NX_RENDERING_KEY__ "rendering"
+
+	private:
+		std::shared_ptr<FrameworkManager<nx::RenderingFrameworkTpl>>	_framework_m;
+
+	public:
+		RenderingSystem();
+		virtual ~RenderingSystem();
+
+	public:
+		void update();
+		void init();
+		bool checkIntegrity() const;
+		nx::RenderingFrameworkTpl* getFramework();
+
+	public:
+		static void event_RenderingEventKey(const nx::Event&);
+	};
+}
+
+#endif
