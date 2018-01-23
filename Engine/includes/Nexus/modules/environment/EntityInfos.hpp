117caa9a4f9df9bacf468275658816dd71a51fe5
Marc-Antoine Leconte <marc-antoine.leconte@epitech.eu>
2018-01-24 00:13:08 +0100

    UseStdAtomic(feat): adding the EntityInfos class

--- Engine/includes/Nexus/modules/environment/EntityInfos.hpp
+++ Engine/includes/Nexus/modules/environment/EntityInfos.hpp
@@ -2,20 +2,54 @@
 #define NEXUS_ENGINE__ENTITYINFOS_HPP_
 
 # include <string>
+# include <atomic>
 
 namespace nx
 {
 	namespace env
 	{
-		struct EntityInfos
+		class EntityInfos
 		{
-			std::string		name;
-			bool			active;
+			std::string			_name;
+			std::atomic<bool>	_active;
 
 		public:
-			EntityInfos(std::string const& name_) : name(name_), active(true) {}
-			EntityInfos(std::string const& name_, bool const active_) : name(name_), active(active_) {}
+			EntityInfos(std::string const& name_) : _name(name_), _active(true) {}
+			EntityInfos(std::string const& name_, bool const active_) : _name(name_), _active(active_) {}
+			EntityInfos() : _name(""), _active(true) {}
+			EntityInfos(const EntityInfos &obj) : _name(obj.getName()), _active(obj.getActive()) {}
 			~EntityInfos() {}
+
+		public:
+			const std::string &	getName() const
+			{
+				return (_name);
+			}
+			bool				getActive() const
+			{
+				return (_active.load());
+			}
+
+		public:
+			void				setName(const std::string &name)
+			{
+				this->_name = name;
+			}
+			void				setActive(const bool active)
+			{
+				this->_active.store(active);
+			}
+
+		public:
+			EntityInfos & operator=(const EntityInfos &other)
+			{
+				if (this != &other)
+				{
+					this->_name = other.getName();
+					this->_active= other.getActive();
+				}
+				return (*this);
+			}
 		};
 	}
 }
