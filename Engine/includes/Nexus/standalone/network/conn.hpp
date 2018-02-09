910eab959469a859442978b59c1dda4e2a73c5b4
JSX <jean-sebastien.herbaux@epitech.eu>
2018-02-09 18:46:26 +0100

    ClientNetwork(feat): Adding conn object

---
+++ Engine/includes/Nexus/standalone/network/conn.hpp
@@ -0,0 +1,22 @@
+#ifndef NEXUS_ENGINE__CONN_HPP_
+#define NEXUS_ENGINE__CONN_HPP_
+
+#include "Nexus/engine.hpp"
+#include "Nexus/standalone/network/netutils.hpp"
+
+namespace nx {
+    class Conn {
+        public:
+            Conn() {}
+            ~Conn() {}
+
+        public:
+            static void localConnect(const std::string& ip, const short port)
+            {
+                external::any infos = nx::netcust_host_t(ip, port);
+                nx::Engine::Instance().emit(nx::EVENT::NETCUST_CONNECT, infos);
+            }
+    };
+}
+
+#endif
