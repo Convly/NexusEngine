409e965284131eb5101fd4f21f36a761d948079c
JSX <jean-sebastien.herbaux@epitech.eu>
2018-02-13 15:04:49 +0100

    PhysicsIntegration(feat): Adding updates functions

--- Engine/test/scripts/PlayerScript.lua
+++ Engine/test/scripts/PlayerScript.lua
@@ -2,8 +2,21 @@ function Init()
     print("Init for playerscript.lua")
     scriptPath = Env:getRessources():getScriptPathAt(0)
     print(scriptPath)
+    print("ALLLLLLLLLLLLLLLLLLL")
+    x = 1
 end
 
 function Update()
-    print("Update for playerscript.lua")
+    print("Update for playerscript.lua", x)
+    x = x + 1
+end
+
+function FixedUpdate()
+    print("Fixedupdate for playerscript.lua", x) 
+    x = x - 1
+end
+
+function LateUpdate()
+    print("LateUpdate for playerscript.lua", x)
+    x = x + 2
 end
