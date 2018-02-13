20096eaed2dfc1f2a36f7ffafa9202d1a73198a9
JSX <jean-sebastien.herbaux@epitech.eu>
2018-02-13 11:07:46 +0100

    ClientNetwork(feat): Remove connection test from the playerscript

--- Engine/test/scripts/PlayerScript.lua
+++ Engine/test/scripts/PlayerScript.lua
@@ -1,7 +1,3 @@
-print("Yolo")
-Conn.localConnect("127.0.0.1", 9898)
-print("Ahyola")
-
 function Init()
     print("Init for playerscript.lua")
     scriptPath = Env:getRessources():getScriptPathAt(0)
